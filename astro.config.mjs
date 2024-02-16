58bd31a8454382ecd0330313c2546827a322e4f3
Kamilo Vasquez <killo.blanco@gmail.com>
2024-02-15 23:45:16 -0500

    ✨ Add tailwind integration to astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-    output: 'server',
-    vite: {
-        optimizeDeps: {
-            exclude: ['oslo']
-        }
+  output: 'server',
+  vite: {
+    optimizeDeps: {
+      exclude: ['oslo']
     }
+  },
+  integrations: [tailwind()]
 });
